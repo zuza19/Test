38cc844aa5aa42e048801df468ae8c7a78eb5495
zuza191 <100830685+zuza19@users.noreply.github.com>
2022-03-03 08:25:25 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define pi 3.14
+int main(void) {
+ int r = 2;
+  double pole=pi*r*r;
+  printf("Pole jest równe %f\n", pole);
+  return 0;
+}
+// komentuje sobie
+/* funkcja scam f i przetestowac (wprowadzanie danych do programu), zrobic 3 pierwsze zadania */
